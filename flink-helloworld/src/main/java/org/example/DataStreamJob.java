96021ef209b442d52f948aa9f3694fa26db02b9b
jingjianqian <18697998680@163.com>
2023-01-31 15:34:05 +0800

    cdc

--- flink-helloworld/src/main/java/org/example/DataStreamJob.java
+++ flink-helloworld/src/main/java/org/example/DataStreamJob.java
@@ -1,68 +1,36 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.example;
 
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 
-/**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+
 public class DataStreamJob {
 
 	public static void main(String[] args) throws Exception {
-		// Sets up the execution environment, which is the main entry point
-		// to building Flink applications.
+		MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+				.hostname("192.168.1.108")
+				.port(3306)
+				.databaseList("school") // set captured database
+				.tableList("school.django_migrations") // set captured table
+				.username("root")
+				.password("123456")
+				.deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+				.build();
+
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.fromElements(1, 2, 3, 4, 5)
-				.map(i -> 2 * i)
-				.print();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
-		 *
-		 * then, transform the resulting DataStream<Long> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide:
-		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
-		 *
-		 */
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
+		env.enableCheckpointing(3000);
+
+
+		env
+			.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+			// set 4 parallel source tasks
+			.setParallelism(4)
+			.print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+		env.execute("Print MySQL Snapshot + Binlog");
+
 	}
 }
